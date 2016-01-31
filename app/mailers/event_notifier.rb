1ee38e8ddbeaa9b7c63051e58ab75a929fd4bdb3
Erica Kantor <erica@ticketevolution.com>
2016-01-31 14:26:13 -0500

    fixed formatiing on event mailer

--- app/mailers/event_notifier.rb
+++ app/mailers/event_notifier.rb
@@ -3,6 +3,6 @@ class EventNotifier < ApplicationMailer
     @event = event
     mail( :to => User.members.collect(&:email),
           :from => creator.email,
-          :subject => "Please Join Us on #{@event.date} for Book Club!" )
+          :subject => "Please Join Us on #{Date.parse(@event.date).strftime("%A %b %d, %Y")} for Book Club!" )
   end
 end
